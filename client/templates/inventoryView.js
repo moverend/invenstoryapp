37efdc03d2e1125c1997753bd01f964ea2721df7
moverend <hello@malcolmo.co>
2015-11-13 15:58:32 -0700

    sets up maintemplate/nav/routes

--- client/templates/inventoryView.js
+++ client/templates/inventoryView.js
@@ -1,5 +1,34 @@
 Template.inventoryView.helpers({
   item: function(){
     return Items.find();
+  },
+  settings: function(){
+    return {
+      collection: Items,
+      showFilter: true,
+      showColumnToggles: true,
+      fields: [
+        {fieldID: 'nameField', key: 'name', label: 'Item'},
+        {key: 'desc', label: 'Description'},
+        {key: 'isRentable', label: 'Rentable'},
+        {key: 'isAvailable', label: 'Available'},
+        {key: 'amount', label: 'Count'},
+        {key: 'customProps', label: 'Custom'},
+      ]
+    };
   }
+});
+
+Template.inventoryView.events({
+  'mouseup .reactive-table tbody tr': function (event) {
+    // Selects the selelected item from the table
+    var selectedItem = this;
+    Session.set('selectedItem', selectedItem);
+  },
+
+  "click #modalAddItem": function(e, template) {
+    e.preventDefault();
+    $('#modalAddItemForm').modal('toggle');
+  }
+
 });
